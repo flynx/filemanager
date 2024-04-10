f229160ace8351a0ee16aef1efa5130ef0863ff7
Alex A. Naanou <alex.nanou@gmail.com>
2024-04-11 00:17:04 +0300

    experimenting with basic navigation...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

--- fm.go
+++ fm.go
@@ -1,3 +1,21 @@
+/*
+* TODO:
+*	- buffer:
+*		- from stdin
+*		- from command
+*		- non-blocking update
+*		- keep position on update
+*			- wait for cur-line in update buffer
+*			- redraw relative to current line
+*	- key bindings:
+*		- reasonable defaults
+*		- config
+*		- action 
+*	- navigation:
+*		- cursor / line / pattern
+*	- 
+*
+*/
 
 package main
 
@@ -7,32 +25,59 @@ import "fmt"
 import "github.com/nsf/termbox-go"
 import "github.com/mattn/go-runewidth"
 
+
+
 var TAB_SIZE = 8
 
 var ROWS, COLS int
-var offsetX, offsetY int
+
+var COL_OFFSET = 0
+var ROW_OFFSET = 0
+
+var CURRENT_ROW= 0
+var CURRENT_ROW_BUF []rune
+
+// XXX cursor mode...
+//		- cursor
+//		- line
+//		- pattern
 
 
-
-var text_buffer = [][]rune {
+var TEXT_BUFFER = [][]rune {
 	{'h','e','l','l','o',},
 	{'\t','w','o','r','l','d','\t','!',},
+	{},
+	{},
+	{'e','n','d','.'},
 }
 func display_text_buffer(){
 	var col, row int
-	var col_offset = 0
 	for row = 0 ; row < ROWS ; row++ {
-		var buf_row = row + offsetY
-		for col = 0 ; col < COLS ; col++ {
-			var buf_col = col + offsetX
-			// XXX can't break lines before an operator???!!
-			if buf_row >= 0 && buf_row < len(text_buffer) && 
-					buf_col >= 0 && buf_col < len(text_buffer[buf_row]) {
+		var buf_row = row + ROW_OFFSET
+		var col_offset = 0
 
-				// XXX need to handle escape sequences (basic state machine -- set bg/fg/...) (???)
+		if(CURRENT_ROW == row){
+			CURRENT_ROW_BUF = TEXT_BUFFER[buf_row] }
+
+		for col = 0 ; col < COLS ; col++ {
+			var buf_col = col + COL_OFFSET
+
+			// mark current row...
+			// XXX line mode...
+			if CURRENT_ROW == row {
+				// XXX make style configurable...
+				termbox.SetBg(col, row, termbox.ColorDefault | termbox.AttrReverse | termbox.AttrBold)
+			} else {
+				termbox.SetBg(col, row, termbox.ColorDefault) }
+
+			// XXX can't break lines before an operator???!!
+			if buf_row >= 0 && buf_row < len(TEXT_BUFFER) && 
+					buf_col >= 0 && buf_col < len(TEXT_BUFFER[buf_row]) {
+
+				// XXX handle escape sequences (basic state machine -- set bg/fg/...)???
 
 				// tab -- offset output to next tabstop... 
-				if text_buffer[buf_row][buf_col] == '\t' {
+				if TEXT_BUFFER[buf_row][buf_col] == '\t' {
 					col_offset += TAB_SIZE - (col % TAB_SIZE)
 					if col_offset == 0 {
 						col_offset = TAB_SIZE
@@ -40,9 +85,8 @@ func display_text_buffer(){
 
 				// normal characters...
 				} else {
-					termbox.SetChar(col + col_offset, row, text_buffer[buf_row][buf_col])
-				}
-			}
+					termbox.SetChar(col + col_offset, row, TEXT_BUFFER[buf_row][buf_col]) }
+			} 
 		}
 	}
 }
@@ -78,6 +122,19 @@ func run_fm(){
 			if  evt.Key == termbox.KeyEsc {
 				termbox.Close() 
 				break } 
+
+			if evt.Key == termbox.KeyArrowUp {
+				// XXX option to skip rows at top...
+				if CURRENT_ROW > 0 {
+					CURRENT_ROW-- } 
+				// XXX scroll the buffer...
+			}
+
+			if evt.Key == termbox.KeyArrowDown {
+				if CURRENT_ROW < ROWS-1 && CURRENT_ROW + ROW_OFFSET < len(TEXT_BUFFER)-1 {
+					CURRENT_ROW++ } 
+				// XXX scroll the buffer...
+			}
 		} }
 
 }
