bc346b8b717a158b14876775519c99ed833b7366
Alex A. Naanou <alex.nanou@gmail.com>
2024-06-18 12:08:30 +0300

    started tweaking cli...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

--- cli.go
+++ cli.go
@@ -2,16 +2,33 @@
 package main
 
 import (
+	"fmt"
+	//"log"
 	//"os"
+	//"slices"
+	//"reflect"
+
 	//"github.com/gdamore/tcell/v2"
+	//"github.com/jessevdk/go-flags"
+
 	//"github.com/mkideal/cli"
 )
 
 
-
+type TcellDrawer struct {
+}
+func (this *TcellDrawer) drawCells(col, row int, str string, style string) {
+	// XXX
+}
 
 func main(){
+	lines := Lines{
+		CellsDrawer: &TcellDrawer{},
+	}
+
+	fmt.Printf("--- %#v\n", lines)
 }
+
 
 
 // vim:set sw=4 ts=4 nowrap :
