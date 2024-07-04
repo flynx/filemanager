b3b27d3e109689f4fc138f7d4fecceaf610e285b
Alex A. Naanou <alex.nanou@gmail.com>
2024-07-04 15:17:35 +0300

    reported https://github.com/golang/go/issues/68308
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

---
+++ experiments/exec_wait_race.go
@@ -0,0 +1,54 @@
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+var RunCount = 1000
+
+func main() {
+	files, _ := os.ReadDir(".")
+	c := len(files) + 2
+	s := 0
+	report := func(n int) {
+		if n != c {
+			fmt.Printf("\nListed: %v of %v (no errors)\n", n, c)
+			s++
+		} else {
+			fmt.Print(".")
+		}
+	}
+	for i := 0; i < RunCount; i++ {
+		n := 0
+		done := make(chan bool)
+		c := exec.Command("ls", "-a")
+		out, _ := c.StdoutPipe()
+		go func() {
+			scanner := bufio.NewScanner(out)
+			for scanner.Scan() {
+				scanner.Text()
+				n++
+			}
+			// handle output...
+			report(n)
+			close(done)
+		}()
+		// start...
+		if err := c.Start(); err != nil {
+			fmt.Println("!!! START:", err)
+		}
+		// XXX this breaks the run script some of the time...
+		if err := c.Wait(); err != nil {
+			fmt.Println("!!! WAIT:", err)
+		}
+		<-done
+	}
+	fmt.Println("")
+	if s > 0 {
+		fmt.Printf("Skipped part of the output %v times of %v\n", s, RunCount)
+	}
+}
