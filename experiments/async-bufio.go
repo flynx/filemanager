d7c127d6928f1e220198afe539ecf387f977fca2
Alex A. Naanou <alex.nanou@gmail.com>
2024-07-09 15:43:54 +0300

    refactoring and cleanup...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

--- experiments/async-bufio.go
+++ experiments/async-bufio.go
@@ -12,35 +12,43 @@ import (
 
 // XXX take either Reader/WriteCloser or Reader/Writer...
 // XXX do we need to control this in any way???
-func PipeTee(reader io.Reader, writer io.WriteCloser, handler func(string)) (<-chan bool) {
+func PipeTee(reader io.Reader, writer io.WriteCloser, handler func(string)) {
 	writer_buf := bufio.NewWriter(writer)
-	done_input := make(chan bool)
-	go func(){
-		scanner := bufio.NewScanner(reader)
-		var output sync.Mutex
-		for scanner.Scan() {
-			txt := scanner.Text()
-			if handler != nil {
-				handler(txt) }
-			// NOTE this needs to be non-blocking this is still blocking...
-			// NOTE seems that bufio is not thread-safe as not explicitly
-			//		sychronising writing and flushing produces unpredictable 
-			//		putput (races)
-			output.Lock()
-			io.WriteString(writer_buf, txt +"\n") 
-			output.Unlock()
-			// keep only one blocked .Flush()...
-			if output.TryLock() {
-				go func(){ 
-					writer_buf.Flush() 
-					output.Unlock() }() } }
-		// finalize things...
+	scanner := bufio.NewScanner(reader)
+	var output sync.Mutex
+	for scanner.Scan() {
+		txt := scanner.Text()
+		if handler != nil {
+			handler(txt) }
+		// NOTE this needs to be non-blocking this is still blocking...
+		// NOTE seems that bufio is not thread-safe as not explicitly
+		//		sychronising writing and flushing produces unpredictable 
+		//		putput (races)
 		output.Lock()
-		writer_buf.Flush()
-		// XXX only do this if WriteCloser...
-		writer.Close()
-		close(done_input) }() 
-	return done_input }
+		io.WriteString(writer_buf, txt +"\n") 
+		output.Unlock()
+		// keep only one blocked .Flush()...
+		if output.TryLock() {
+			go func(){ 
+				writer_buf.Flush() 
+				output.Unlock() }() } }
+	// finalize things...
+	output.Lock()
+	writer_buf.Flush()
+	// XXX only do this if WriteCloser...
+	writer.Close() }
+
+
+// XXX make this a generic Async(func, ...args)
+func AsyncPipeTee(reader io.Reader, writer io.WriteCloser, handler func(string)) (<-chan bool) {
+	done := make(chan bool)
+	go func(){ 
+		PipeTee(reader, writer, handler)
+		close(done) }()
+	return done }
+
+
+
 
 
 func main(){
@@ -48,7 +56,7 @@ func main(){
 	ir, iw := io.Pipe()
 	or, ow := io.Pipe()
 
-	done_input := PipeTee(ir, ow, 
+	done_input := AsyncPipeTee(ir, ow,
 		func(s string){
 			fmt.Println(">>>", s) })
 
