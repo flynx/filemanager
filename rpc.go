a9e3768cb2c5c8d0566033d819877a3a5bc053c7
Alex A. Naanou <alex.nanou@gmail.com>
2024-08-12 16:32:07 +0300

    thinking about rpc/ipc control...
    Signed-off-by: Alex A. Naanou <alex.nanou@gmail.com>

---
+++ rpc.go
@@ -0,0 +1,12 @@
+
+package lines
+
+import (
+)
+
+// XXX seems that http is the fashionable ipc in Go, do do we need to 
+//		make it rpc (gRPC?) or go towards an honest REST API?
+// XXX avoid go's "net/rpc" -- buggy/frozen...
+
+
+// vim:set sw=4 ts=4 nowrap :
